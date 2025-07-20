24c0463681aa8b38f1d2f8d183631348f7ac7178
cisco-03 <146370669+cisco-03@users.noreply.github.com>
2025-07-20 20:49:12 +0200

    Configuration Firebase

---
+++ firebase-config.ts
@@ -0,0 +1,15 @@
+// Configuration Firebase pour la production
+// Ce fichier contient les clés publiques Firebase qui peuvent être exposées côté client
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBM_8bJBpCOzT3PnjzjQvlhDb5vS6AUEJ8",
+  authDomain: "florasynth-a461d.firebaseapp.com",
+  projectId: "florasynth-a461d",
+  storageBucket: "florasynth-a461d.firebasestorage.app",
+  messagingSenderId: "1040083472841",
+  appId: "1:1040083472841:web:2d5429cfefbb9eac7c1818",
+  measurementId: "G-3G3YNB8GKY"
+};
+
+// Note: Ces clés sont publiques et sécurisées par les règles Firebase
+// Elles peuvent être exposées côté client sans risque de sécurité
